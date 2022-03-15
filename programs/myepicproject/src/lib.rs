18439003beaf3f298c9a911da1a4b6a21d194442
Mugen <paradigmcomplex@gmail.com>
2022-03-15 16:43:42 -0500

    first deployment

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6djRjPLkyXiuSaZWCsAnvYiSEytyGWi55q9NranExu9K");
 
 #[program]
 pub mod myepicproject {
